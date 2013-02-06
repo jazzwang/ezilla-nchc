6075e08ac1d3d62c75d79bb9ab90dba341f0f726
jazzwang <jazzwang@6d666bca-7c83-4fba-b8d7-87074b371b7b>
2013-02-06 17:01:15 +0000

    * add install_xwin.sh
    git-svn-id: https://ezilla-nchc.svn.sf.net/svnroot/ezilla-nchc@43 6d666bca-7c83-4fba-b8d7-87074b371b7b

---
+++ d-i/squeeze/install_xwin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+aptitude -y install xorg
+aptitude -y install gnome
+aptitude -y install xfonts-wqy
+aptitude -y install ttf-arphic-bkai00mp ttf-arphic-uming
+aptitude -y install gcin gcin-qt3-immodule im-switch
